c117c277bad4bc242d4b774bd31657194a735780
shantanu <shantanubansode0@gmail.com>
2024-10-07 02:10:26 +0530

    Add all the files

---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,19 @@
+package Oct.ex_03102024;
+
+public class Lab002 {
+     public static void main(String[] args) {
+        int a=10;
+        System.out.println("Output:/n"+"TableofTen");
+        System.out.println("Multiplication");
+        System.out.println("10*1=" + a);
+        System.out.println("10*2=" + a*2);
+        System.out.println("10*3=" + a*3);
+        System.out.println("10*4=" + a*4);
+        System.out.println("10*5=" + a*5);
+        System.out.println("10*6=" + a*6);
+        System.out.println("10*7=" + a*7);
+        System.out.println("10*8=" + a*8);
+        System.out.println("10*9=" + a*9);
+        System.out.println("10*10=" + a*10);
+    }
+}
---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,20 @@
+package Oct.ex_03102024;
+
+import java.sql.SQLOutput;
+
+public class Lab003 {
+    public static void main(String[] args){
+
+        System.out.print("Addition: ");
+        System.out.println(10+2);
+        System.out.print("Subtraction: ");
+        System.out.println(10-2);
+        System.out.print("Multiplication: ");
+        System.out.println(10*2);
+        System.out.print("Division: ");
+        System.out.println(10/2);
+        System.out.print("Modulus: ");
+        System.out.println(10%2);
+
+    }
+}
