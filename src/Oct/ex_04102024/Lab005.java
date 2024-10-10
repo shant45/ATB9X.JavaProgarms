89f8a9aac0ced065e169916f4bbf0ffb6473a3e6
shantanu <shantanubansode0@gmail.com>
2024-10-11 02:38:55 +0530

    Add all the files

---
+++ src/Oct/ex_04102024/Lab005.java
@@ -0,0 +1,14 @@
+package Oct.ex_04102024;
+
+public class Lab005 {
+    public static class lab005 {
+        public static void main(String[] args) {
+            int table = 4;
+            System.out.printf("%d x %d = %d", table, 1, table);
+            System.out.println();
+            System.out.printf("%d x %d = %d", table, 2, table*2);
+            System.out.println();
+            System.out.printf("%d x %d = %d", table, 3, table*3);
+        }
+    }
+}
